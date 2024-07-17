2566a7ffc093e3bd636569c9d29eed2fbe3a4741
SS80194 <ss80194@outlook.com>
2024-07-17 11:39:29 +0800

    用来build的东西补充

---
+++ StringC.cpp
@@ -0,0 +1,58 @@
+#include"StringC.hpp"
+
+const int StringC::max_length=64;
+StringC::StringC(char *tar_c)
+{
+    for(int i=0;i<=max_length&&tar_c[i]!=0;i++)
+        s[i]=tar_c[i];
+}
+StringC::StringC(std::string ori_s)
+{
+    const char *p=ori_s.c_str();
+    std::strcpy(s,p);
+}
+ull StringC::toHash()
+{
+    ull ans=0,bss=1;
+    for(int i=0;i<=max_length&&s[i]!=0;i++)
+        ans+=bss*(s[i]-'a'),bss*=26;
+    return ans;
+}
+bool operator<(StringC a,StringC b)
+{
+    for(int i=0;i<=StringC::max_length;i++)
+    {
+        if(b.s[i]==0) return false;
+        if(a.s[i]==0) return true;
+        if(a.s[i]<b.s[i]) return true;
+        if(a.s[i]>b.s[i]) return false;
+    }
+    return false;
+}
+bool operator==(StringC a,StringC b)
+{
+    for(int i=0;i<=a.max_length;i++)
+    {
+        if(b.s[i]==0&&a.s[i]==0) break;
+        if(a.s[i]!=b.s[i]) return false;
+    }
+    return true;
+}
+int StringC::strlen()
+{
+    for(int i=0;i<=max_length;i++)
+        if(!s[i]) return i;
+    return max_length;
+}
+bool StringC::validSetA()
+{
+    for(int i=0;i<=max_length;i++)
+    {
+        if(s[i]==0) break;
+        bool flg=false;
+        if(isNumber(s[i])||isLetter(s[i])||s[i]=='_')
+            flg=true;
+        if(!flg) return false;
+    }
+    return true;
+}
