1f7dc162d116428964fae92ad5ca17192a9b866b
SS80194 <ss80194@outlook.com>
2024-07-17 09:39:51 +0800

    console panel debug

--- ConsolePanel.hpp
+++ ConsolePanel.hpp
@@ -18,6 +18,7 @@ class ConsolePanel
         std::getline(std::cin,s_ori);
         std::stringstream s_flow(s_ori);
         std::vector<std::string>().swap(s);
+        //本质上是vector的s.clear() 使用swap是为了防止内存泄漏
         std::string temp;
         while(s_flow>>temp) {s.push_back(temp);}
     }
@@ -29,7 +30,7 @@ class ConsolePanel
     int size(){return s.size();}
     bool safetyCheck(){for(std::string &x:s) if(x.size()>StringC::max_length) return false;return true;}
     void invalidOperation(){printf("Invalid\n");fflush(stdout);return ;}
-    std::string operator[](int k){if(k>=size()) return 0;return s[k];}
+    std::string operator[](int k){if(k>=size()) return "";return s[k];}
     std::string type(){return (size()?s[0]:"");}
 };
 
